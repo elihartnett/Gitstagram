5369579c012fcea3cbf5db31c2e05a04bf6d521d
elichartnett <elichartnett@icloud.com>
2022-07-29 18:42:15 -0500

    Search screen and icon update

---
+++ Gitstagram/AssetConstants.swift
@@ -0,0 +1,17 @@
+//
+//  AssetConstants.swift
+//  Gitstagram
+//
+//  Created by Eli Hartnett on 7/29/22.
+//
+
+import Foundation
+
+struct AssetConstants {
+    
+    struct Images {
+        static let appLogo = "logo"
+        static let emptyStateLogo = "empty-state-logo"
+        static let avatarPlaceholder = "avatar-placeholder"
+    }
+}
--- Gitstagram/Reusable Views/GGTextField.swift
+++ Gitstagram/Reusable Views/GGTextField.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GFTextField: UITextField {
+class GGTextField: UITextField {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
