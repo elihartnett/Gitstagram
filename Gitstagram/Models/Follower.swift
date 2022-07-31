624a515afb36810aa6d4b5d6b6869b3c954fc2f4
elichartnett <elichartnett@icloud.com>
2022-07-31 10:09:08 -0500

    Network models

---
+++ Gitstagram/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Gitstagram
+//
+//  Created by Eli Hartnett on 7/31/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
