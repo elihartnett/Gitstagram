3b6a634175a057922052b8c20e5b3c22be350150
elichartnett <elichartnett@icloud.com>
2022-08-06 11:47:08 -0500

    Item info VC superclass

---
+++ Gitstagram/Controllers/GGItemInfoVC.swift
@@ -0,0 +1,55 @@
+//
+//  GGItemInfoVC.swift
+//  Gitstagram
+//
+//  Created by Eli Hartnett on 8/6/22.
+//
+
+import UIKit
+
+class GGItemInfoVC: UIViewController {
+
+    let stackView = UIStackView()
+    let itemInfoView1 = GGItemInfoView()
+    let itemInfoView2 = GGItemInfoView()
+    let actionButton = GGButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.addArrangedSubview(itemInfoView1)
+        stackView.addArrangedSubview(itemInfoView2)
+    }
+}
