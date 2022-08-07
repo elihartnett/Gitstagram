4c5a9dc21704757bdc3f54b9344e50b733ab9bbd
elichartnett <elichartnett@icloud.com>
2022-08-07 11:57:58 -0500

    Persistence manager

---
+++ Gitstagram/Managers/PersistenceManager.swift
@@ -0,0 +1,74 @@
+//
+//  PersistenceManager.swift
+//  Gitstagram
+//
+//  Created by Eli Hartnett on 8/7/22.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add
+    case remove
+}
+
+struct PersistenceManager {
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys: String {
+        case favorites = "favorites"
+    }
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GGError?) -> Void) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    retrievedFavorites.append(favorite)
+                case .remove:
+                    retrievedFavorites.removeAll { follower in
+                        follower.login == favorite.login
+                    }
+                }
+                
+                completed(saveFavorites(favorites: retrievedFavorites))
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GGError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites.rawValue) as? Data else {
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToRetrieveFavorites))
+        }
+    }
+    
+    static func saveFavorites(favorites: [Follower]) -> GGError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites.rawValue)
+            return nil
+        }
+        catch {
+            return .unableToSaveFavorites
+        }
+    }
+}
--- Gitstagram/Utilities/GGError.swift
+++ Gitstagram/Utilities/GGError.swift
@@ -8,8 +8,13 @@
 import Foundation
 
 enum GGError: String, Error {
-    case invalidUsername = "This username created an invalid request"
-    case unableToComplete = "Unable to complete your request. Please check your internet connection"
+    case invalidUsername = "This username created an invalid request."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidData = "The data received from the server was invalid. Please try again."
     case invalidResponse = "Invalid response from the server. Please try again."
+    
+    case unableToRetrieveFavorites = "There was an error retrieving favorites. Please try again."
+    case unableToSaveFavorites = "There was an error saving favorites. Please try again."
+    case unableToFavorite = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You have already favorited this user."
 }
